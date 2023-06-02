e113da16e84f6f3a5ddc5dcc69cf439ca16ccd77
elleinadseer <126515415+elleinadseer@users.noreply.github.com>
2023-06-02 18:48:41 +0100

    template for questions

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('/Users/danielle/bootcamp/readme-generator-w9/utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// Questions: Project Title, Description, Table of Contents, Installation, Usage, License, Contributing, 
+// Tests, and Questions
+
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of your project?'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
